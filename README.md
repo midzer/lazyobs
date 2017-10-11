92926d03aeadd29b8ed7ec28237b0d0a4d61dfab
midzer <midzer@gmail.com>
2017-10-11 21:17:15 +0200

    update lazyops to match demo site

--- README.md
+++ README.md
@@ -1,17 +1,39 @@
 # lazyobs
-Lazy load any images and videos with modern Intersection Observer API.
+Lazy load any images, videos and (heavy) components needing individual JavaScipt with Intersection Observer API.
 
 ## Demo
-lazyobs is used at https://feuerwehr-eisolzried.de/media/
+lazyobs is used at https://feuerwehr-eisolzried.de/
+Video example is at https://feuerwehr-eisolzried.de/media/
 
 ## How to use
 Simply include `<script src="/js/lazyobs.min.js" async></script>` right before `</body>`.
 
-Add `lazy` class to any image or video below the fold. I recommend skipping elements in inital viewport, because you want those to load as fast as possibe. Lazy loading kicks in immediately for new content entering viewport after scroll or any other visible UI change.
+Add `lazy` class to any image, video or component which needs JS to operate.
+
+Don't forget to put and `id=COMPONENT` for your lazy components which will point to your `/assets/js/COMPONENT.js`. For example Demo calendar HTML snippet is `<div id="calendar" class="lazy"></div>` which lazy loads JS file `calendar.js` via Promise.
+
+Lazy loading kicks in right before for new content entering viewport after scroll or any other visible UI change.
+
+## Animation
+Maybe you want to animate your images after being loaded?
+
+Just put following code to your css:
+```
+.lazy {
+  opacity: 0;
+  will-change: opacity;
+}
+
+.loaded {
+  animation: fadeIn 0.5s ease-in;
+  animation-fill-mode: forwards;
+}
+
+@keyframes fadeIn {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+```
 
 ## Browser support
-Currently Chrome > 56, Firefox > 55, Edge 15.
-For recent support check https://caniuse.com/#search=IntersectionObserver
-
-## Fallback
-If Intersection Observer is not supported in your browser, all lazy elements are loaded at once.
+Currently Chrome > 51, Firefox > 55, Edge 15. For recent support check [here](https://caniuse.com/#search=IntersectionObserver). Use [polyfill](https://github.com/w3c/IntersectionObserver/tree/master/polyfill) to support older browsers.
--- lazyobs.js
+++ lazyobs.js
@@ -1,56 +1,77 @@
-// Lazy load images and videos
-// 1. Convert node list of all elements with data-src attributed to array
-var els = document.querySelectorAll('.lazy');
-if (els.length > 0) {
-    function replaceSrc(element) {
-        element.src = element.dataset.src;
-        element.removeAttribute('data-src');
-    }
-    function addLoaded(element) {
-        element.classList.add('loaded');
-    }
-    function load(element) {
-        if (element.nodeName == 'VIDEO') {
-            var sources = element.getElementsByTagName('source');
-            for (let i = 0; i < sources.length; i++) {
-                replaceSrc(sources[i]);
-            }
-            element.load();
-            element.onloadstart = function() { addLoaded(element) };
-        }
-        else {
-            replaceSrc(element);
-            element.onload = function() { addLoaded(element) };
-        }
-    }
+function query(selector) {
+    return Array.from(document.querySelectorAll(selector));
+}
 
-    if (! ('IntersectionObserver' in window)) {
-        console.log('Intersection Observer not supported');
-        for (var i = 0; i < els.length; i++) {
-            load(els[i]);
+function replaceSrc(element) {
+    element.src = element.dataset.src;
+    element.removeAttribute('data-src');
+}
+
+function removeHint() {
+    this.style.willChange = 'auto';
+}
+
+function addLoaded(element) {
+    element.addEventListener('animationend', removeHint);
+    element.classList.add('loaded');
+}
+
+function loadScript(src) {
+    return new Promise(function(resolve, reject) {
+        const script = document.createElement('script');
+        script.async = true;
+        script.src = '/assets/js/' + src.id + '.js';
+        script.onload = function() {
+            resolve(script.src);
+            addLoaded(src);
+        };
+        script.onerror = reject;
+        if (document.head.lastChild != script) {
+            document.head.appendChild(script);
         }
-    } else {
-    // 2. Create the IntersectionObserver and bind it to the function we want it to work with
-    var observer = new IntersectionObserver(onChange, {
-        threshold: 0.5
     });
-    
-    function onChange(changes) {
-        // 3. For each element that we want to change
-        changes.forEach(change => {
-            // Edge 15 doesn't support isIntersecting, but we can infer it from intersectionRatio
-            // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/12156111/
-            if (change.intersectionRatio > 0) {
-                // 4. take url from `data-src` attribute
-                load(change.target);
+}
 
-                // 5. Stop observing the current target
-                observer.unobserve(change.target);
-            }
-        })
+function load(element) {
+    if (element.nodeName == 'VIDEO') {
+        // <video> element
+        element.onloadstart = function() { addLoaded(element) };
+        var sources = element.getElementsByTagName('source');
+        for (let i = 0; i < sources.length; i++) {
+            replaceSrc(sources[i]);
+        }
+        element.load();
     }
-    // 6. Observe each image derived from the array above
-    els.forEach(el => observer.observe(el));
+    else if (element.nodeName == 'IMG') {
+        // <img> element
+        element.onload = function() { addLoaded(element) };
+        replaceSrc(element);
+    }
+    else if (element.hasAttribute('id')) {
+        // any element with an id
+        loadScript(element);
     }
 }
 
+// Pre-load items that are within 150px of the visible viewport height.
+var observer = new IntersectionObserver(function(changes) {
+    changes.forEach(function(change) {
+        // Edge 15 doesn't support isIntersecting, but we can infer it
+        // https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/12156111/
+        // https://github.com/WICG/IntersectionObserver/issues/211
+        const isIntersecting = (typeof change.isIntersecting === 'boolean') ?
+        change.isIntersecting : change.intersectionRect.height > 0;
+        if (isIntersecting) {
+            load(change.target);
+
+            // 5. Stop observing the current target
+            observer.unobserve(change.target);
+        }
+    });
+  },
+  { rootMargin: "150px 0px" }
+);
+
+query(".lazy").forEach(function(item) {
+    observer.observe(item);
+});
